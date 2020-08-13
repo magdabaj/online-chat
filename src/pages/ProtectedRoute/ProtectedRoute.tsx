2eb9952199bdf77da73f83c5e190a1c9b314cd0b
magda <magdabaj>
2020-08-13 13:55:56 +0200

    Added protected route

---
+++ src/pages/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,31 @@
+import {Component, FC, useContext} from "react";
+import React from "react";
+import {Route, Redirect, RouteProps} from "react-router-dom";
+
+export interface ProtectedRouteProps extends RouteProps {
+    isAuthenticated: boolean;
+    authenticationPath: string;
+}
+
+export class ProtectedRoute extends Route<ProtectedRouteProps>{
+    public render() {
+        let redirectPath: string = ''
+        if (!this.props.isAuthenticated) redirectPath = this.props.authenticationPath
+
+        if (redirectPath) {
+            const redirectComponent = () => (<Redirect to={{pathname: redirectPath}}/>)
+            return <Route {...this.props} component={renderComponent} render={undefined}/>
+        } else return <Route {...this.props}/>
+    }
+}
+
+// const ProtectedRoute: FC = ({component: Component, ...rest}: any) => {
+//     const { state } = useContext({isAuth: false})
+//
+//     return(
+//         <Route render={props =>
+//             !state.isAuth ? <Redirect to={'/'}/> : <Component {...props}/>
+//         }
+//             {...rest}/>
+//     )
+// }
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -94,12 +94,6 @@ const Register: FC = () => {
                                 autoComplete="current-password"
                             />
                         </Grid>
-                        {/*<Grid item xs={12}>*/}
-                        {/*    <FormControlLabel*/}
-                        {/*        control={<Checkbox value="allowExtraEmails" color="primary" />}*/}
-                        {/*        label="I want to receive inspiration, marketing promotions and updates via email."*/}
-                        {/*    />*/}
-                        {/*</Grid>*/}
                     </Grid>
                     <Button
                         type="submit"
